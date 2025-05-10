672b3f9fca16a85e2aa6cffb44bf113230b3845e
RyuYamaki <ryuyamaki@RyuYamakinoMacBook-Pro.local>
2025-05-10 10:57:22 +0900

    next.config fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  headers: async () => [
+    {
+      source: "/(.*)", // すべてのパスに適用
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "no-store", // キャッシュ無効化
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
